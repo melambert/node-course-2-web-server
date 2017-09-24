1a62adc58f129e6ef61980dc0a383a398d5c917f
Mark <mark.e.lambert@googlemail.com>
2017-09-24 17:57:38 +0100

    Added projects page

--- dist/server.js
+++ dist/server.js
@@ -50,6 +50,12 @@ app.get('/about', function (req, res) {
     });
 });
 
+app.get('/projects', function (req, res) {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
+
 app.get('/bad', function (req, res) {
     res.send({
         errorMessage: 'Unable to fulfill request'
